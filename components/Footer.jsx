261be18a7b866e22b26c676941fcfd72e559e1cf
unknown <kingsimpa69@gmail.com>
2024-07-04 09:35:38 -0600

    Lessss go Magic Eden!

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
                 <Link href={"https://twitter.com/based_fellas"} target={"_blank"}><FontAwesomeIcon icon="fa-brands fa-x-twitter" /></Link>
                 <Link href={"https://discord.com/invite/EVk2Zk2N3z"} target={"_blank"}><FontAwesomeIcon icon="fa-brands fa-discord" /></Link>
                 <Link href={"https://t.me/OfficialBasedFellas"} target={"_blank"}><FontAwesomeIcon icon="fa-brands fa-telegram" /></Link>
-                <Link href={"https://opensea.io/collection/based-fellas"} target={"_blank"}><Image priority alt={"opensealogo"} src={'/images/opensea.png'} width={25} height={25} /></Link>
+                <Link href={"https://magiceden.io/collections/base/0x217ec1ac929a17481446a76ff9b95b9a64f298cf"} target={"_blank"}><Image priority alt={"melogo"} src={'/images/magiceden.png'} width={25} height={25} /></Link>
                 <Link href={"https://basescan.org/token/0x217ec1ac929a17481446a76ff9b95b9a64f298cf"} target={"_blank"}><Image priority alt={"basescanlogo"} src={'/images/basescan.png'} width={25} height={25} /></Link>             
                 <Link href={"https://base.blockscout.com/token/0x217Ec1aC929a17481446A76Ff9B95B9a64F298cF"} target={"_blank"}><Image priority alt={"blockscoutlogo"} src={'/images/blockscout.png'} width={25} height={25} /></Link>      
                 <Link href={"https://github.com/KingSimpa69"} target={"_blank"}><FontAwesomeIcon icon="fa-brands fa-github" /></Link>
